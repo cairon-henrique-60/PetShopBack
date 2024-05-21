31bc220ef7ea0f6cd273e271cd983ad4a9ec1041
Gustavo Augusto <81970233+Guxxtavoww@users.noreply.github.com>
2024-05-20 21:47:10 -0300

    Chore: adding calendar module

--- src/modules/pet/controllers/pet.controller.ts
+++ src/modules/pet/controllers/pet.controller.ts
@@ -14,13 +14,13 @@ import { orderByFields } from 'src/config/swagger.config';
 import { UuidParam } from 'src/shared/decorators/uuid-param.decorator';
 import { DecodedToken } from 'src/shared/decorators/decoded-token.decorator';
 import { ApiPaginationQuery } from 'src/shared/decorators/api-pagination-query.decorator';
+import { DataBaseInterceptor } from 'src/lib/http-exceptions/errors/interceptors/database.interceptor';
 
 import { PetService } from '../services/pet.service';
 import { CreatePetDTO } from '../dtos/create-pet.dto';
 import { UpdatePetDTO } from '../dtos/update-pet.dto';
 import { ListPetsQuerysDTO } from '../dtos/list-pets-querys.dto';
 import { PaginatePetsQuerysDTO } from '../dtos/paginate-pets-querys.dto';
-import { DataBaseInterceptor } from 'src/lib/http-exceptions/errors/interceptors/database.interceptor';
 
 @ApiTags('pet')
 @Controller('pet')
