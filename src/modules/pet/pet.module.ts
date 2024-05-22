d2b45d148ad3051bb3a10adf91765c4b89db2da3
Gustavo Augusto <81970233+Guxxtavoww@users.noreply.github.com>
2024-05-22 14:49:32 -0300

    Fix: fixing import alias for tests

--- src/modules/pet/pet.module.ts
+++ src/modules/pet/pet.module.ts
@@ -1,17 +1,12 @@
 import { Module } from '@nestjs/common';
 
-import { PetSpeciesService } from '../pet-species/services/pet-species.service';
-
-import { PetBreedService } from '../pet-breed/services/pet-breed.service';
-
-import { UserService } from '../user/services/user.service';
+import { UserModule } from '../user/user.module';
+import { PetBreedModule } from '../pet-breed/pet-breed.module';
+import { PetSpeciesModule } from '../pet-species/pet-species.module';
 
 import { PetService } from './services/pet.service';
 import { petProvider } from './providers/pet.provider';
 import { PetController } from './controllers/pet.controller';
-import { PetSpeciesModule } from '../pet-species/pet-species.module';
-import { PetBreedModule } from '../pet-breed/pet-breed.module';
-import { UserModule } from '../user/user.module';
 
 const providers = [...petProvider, PetService]
 
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -4,9 +4,11 @@ import { UserService } from './services/user.service';
 import { userProvider } from './providers/user.provider';
 import { UserController } from './controllers/user.controller';
 
+const providers = [...userProvider, UserService]
+
 @Module({
   controllers: [UserController],
-  providers: [...userProvider, UserService],
-  exports: [...userProvider, UserService],
+  providers,
+  exports: providers,
 })
 export class UserModule {}
